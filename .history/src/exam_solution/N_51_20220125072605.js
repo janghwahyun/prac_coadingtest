1afc716b27ef6fb3aefc65b63bdd66a8f8e98bff
janghwahyun <1117jhh@gmail.com>
2022-01-26 23:53:18 +0900

    51, 52

---
+++ .history/src/exam_solution/N_51_20220125072605.js
@@ -0,0 +1,18 @@
+// 순서가 이렇게 됨
+// [1, 3, 5, 4, 8, 6, 7, 2]
+// [1, 3, 5, 4], [8, 6, 7, 2]
+// [1], [3], [5], [4], [8], [6], [7], [2]
+// [1, 3], [4, 5], [6, 8], [2, 7]
+// [1, 3, 4, 5], [2, 6, 7, 8]
+// [1, 2, 3, 4, 5, 6, 7, 8]
+
+// 분할부분
+function mergeSort() {}
+
+// 정렬하고 합치는 부분
+function merge() {}
+
+// const array = prompt().split().map(i => parseInt(n, 10));
+
+console.log(array);
+console.log(mergeSort(array));
